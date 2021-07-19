047a6871f6f1fd7fcb7efbdb70bad86ddea1dbff
Alexmiln <alexmiln04@gmail.com>
2021-07-19 16:46:05 +0300

    Moved getRandomInt to a separate class

--- src_test/Apple.ts
+++ src_test/Apple.ts
@@ -1,25 +1,17 @@
-import {GameConfig} from "./Config";
 import Coordinate from "./Coordinate";
 
 export default class Apple {
     private position: Coordinate;
 
     constructor() {
-        this.position = new Coordinate(16, 10);
+        this.position = new Coordinate(17, 4);
     }
 
-    public spawn(): Coordinate
-    {
-        this.position = new Coordinate(this.getRandomInt(GameConfig.width, GameConfig.height), this.getRandomInt(GameConfig.width, GameConfig.height));
-
-        return this.position;
+    public setPosition(position: Coordinate): void {
+        this.position = position;
     }
 
     public getPosition(): Coordinate {
         return this.position;
-    }
-
-    private getRandomInt(min: number, max: number) {
-        return Math.floor(Math.random() * (max - min) + min);
     }
 }
